add8baf6e2ed898a7a2dee15c72ba00d2bcc5e59
flora_tyh <flora_tyh>
2019-08-14 19:10:38 +0800

    aiax city

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -9,60 +9,25 @@
             </div>
           </div>
         </div>
-        <div class="area">
-          <div class="title border-topbottom">热门城市</div>
-          <div class="button-list">
-            <div class="button-wrapper">
-              <div class="button">四川</div>
+        <div class="area" >
+          <div class="title border-topbottom"  >热门城市</div>
+          <div class="button-list" >
+            <div class="button-wrapper" v-for="items in hotCities" :key="items.id">
+              <div class="button">{{items.name}}</div>
             </div>
           </div>
         </div>
-        <div class="area ">
-          <div class="title border-topbottom">A</div>
+        <div class="area " v-for="(item,key) of cities" :key="key">
+          <div class="title border-topbottom" >{{key}}</div>
             <div class="item-list">
-              <div class="item border-bottom">aaaa</div>
-              <div class="item">aaaa</div>
-              <div class="item">aaaa</div>
-              <div class="item">aaaa</div>
-              <div class="item">aaaa</div>
-              <div class="item">aaaa</div>
+              <div
+                class="item border-bottom"
+                v-for="innerItem of item"
+                :key="innerItem.id"
+              >
+                {{innerItem.name}}
+              </div>
             </div>
-          <div class="title border-topbottom">A</div>
-          <div class="item-list">
-            <div class="item border-bottom">aaaa</div>
-            <div class="item">aaaa</div>
-            <div class="item">aaaa</div>
-            <div class="item">aaaa</div>
-            <div class="item">aaaa</div>
-            <div class="item">aaaa</div>
-          </div>
-          <div class="title border-topbottom">A</div>
-          <div class="item-list">
-            <div class="item border-bottom">aaaa</div>
-            <div class="item">aaaa</div>
-            <div class="item">aaaa</div>
-            <div class="item">aaaa</div>
-            <div class="item">aaaa</div>
-            <div class="item">aaaa</div>
-          </div>
-          <div class="title border-topbottom">A</div>
-          <div class="item-list">
-            <div class="item border-bottom">aaaa</div>
-            <div class="item">aaaa</div>
-            <div class="item">aaaa</div>
-            <div class="item">aaaa</div>
-            <div class="item">aaaa</div>
-            <div class="item">aaaa</div>
-          </div>
-          <div class="title border-topbottom">A</div>
-          <div class="item-list">
-            <div class="item border-bottom">aaaa</div>
-            <div class="item">aaaa</div>
-            <div class="item">aaaa</div>
-            <div class="item">aaaa</div>
-            <div class="item">aaaa</div>
-            <div class="item">aaaa</div>
-          </div>
         </div>
       </div>
     </div>
@@ -72,6 +37,10 @@
 import Bscroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+    cities: Object,
+    hotCities: Array
+  },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
   }
